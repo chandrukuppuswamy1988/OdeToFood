9998e5c885610fe778b98ad9589e8a48499d6597
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2019-08-24 21:10:24 +0530

    Chapter 1 completed

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public IEnumerable<Restaurant> restaurants { get; set; }
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant(){ Id = 1,Name = "Saravana Bhavan",Cuisine=CuisineType.Indian ,Location="New Jersy"},
+                new Restaurant(){ Id = 1,Name = "Mary Land",Cuisine=CuisineType.Italian,Location="San Fransico"},
+                new Restaurant(){ Id = 1,Name = "Cinnomon Club",Cuisine=CuisineType.Mexican,Location="London"},
+                new Restaurant(){ Id = 1,Name = "La Costa",Cuisine=CuisineType.Mexican,Location="Portugal"}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+           
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+using OdeToFood.Core;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         public IConfiguration config { get; set; }
+        public IEnumerable<Restaurant> restaurants { get; set; }
         public string Message { get; set; }
+        public IRestaurantData restaurantData {get;set;}
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
-            
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
+            this.restaurants = this.restaurantData.GetAll();
             this.Message = this.config["Message"];
         }
     }
